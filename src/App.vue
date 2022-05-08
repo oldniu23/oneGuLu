922b382488caae589e0c446cdcddca16bc22a3b5
oldniu <1241677287@qq.com>
2022-05-08 22:27:57 +0800

    新增切换红框 完善aside切换逻辑

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,9 @@ export default {
   name: "App",
   //控制aside的隐藏和显示 provide和inject
   setup() {
-    const menuVisible = ref(false);
+    // 手机端和pc端要不一样的值  pc端不需要切换aside 值也就永远是true
+    const width = document.documentElement.clientWidth;
+    const menuVisible = ref(width <= 500 ? false : true);
     // 任何人使用menuVisible只需要提供xxx这个名字
     provide("menuVisible", menuVisible);
   },
