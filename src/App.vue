35400a5405532dae0f32ecb1e552c48ce0b9aa41
oldniu <1241677287@qq.com>
2022-05-08 18:53:02 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    hi
+    <oldniu />
+  </div>
+</template>
+
+<script>
+import oldniu from "./components/oldniu.vue";
+export default {
+  name: "App",
+  components: {
+    oldniu,
+  },
+};
+</script>
