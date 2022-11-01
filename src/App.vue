dd15aa019fd3016a13db5fd37c6b4c2caa519630
oldniu <1241677287@qq.com>
2022-11-01 23:51:25 +0800

    路由切换后侧边栏隐藏

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@
 
 <script lang='ts'>
 import { ref, provide } from "vue";
+import { router } from './router';
+
 export default {
   name: "App",
   //控制aside的隐藏和显示 provide和inject
@@ -13,6 +15,12 @@ export default {
     const menuVisible = ref(width <= 500 ? false : true);
     // 任何人使用menuVisible只需要提供xxx这个名字
     provide("menuVisible", menuVisible);
+    router.afterEach(()=> {
+      // 路由切换后手机端直接隐藏Doc的侧边栏
+      if( width <= 500 ) {
+        menuVisible.value = false;
+      }
+    })
   },
 };
 </script>
---
+++ src/router.ts
@@ -0,0 +1,28 @@
+import { createWebHashHistory, createRouter } from "vue-router";
+import Home from "./views/Home.vue";
+import Doc from "./views/Doc.vue";
+import SwitchDemo from "./components/SwitchDemo.vue"
+
+const history = createWebHashHistory();
+export const router = createRouter({
+  history: history,
+  routes: [
+    {
+      path: "/",
+      component: Home,
+    },
+    {
+      path: "/doc",
+      component: Doc,
+      children:[
+        {path: 'switch', component: SwitchDemo }
+      ]
+    },
+  ],
+});
+
+// 路由切换后执行
+router.afterEach(()=>{
+  console.log("切换路由了");
+  
+})
