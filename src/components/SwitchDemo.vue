f063e0e140134af59979d138dbdff864bb9f6f25
oldniu <1241677287@qq.com>
2022-12-26 17:00:02 +0800

    switch组件 按钮hover后右滚

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <Switch />
+        <button><span></span></button>
     </div>
 </template>
 <script>
@@ -9,3 +9,29 @@ export default {
     components: {Switch},
 }
 </script>
+
+<style lang="scss" scoped>
+    $h: 22px;
+    $h2: $h - 4px;
+    button{
+        height: $h;
+        width: $h*2;
+        border: none;
+        background: blue;
+        border-radius: $h/2;
+        position: relative;
+    }
+    span{
+        position: absolute;
+        top: 2px;
+        left: 2px;
+        height: $h2;
+        width: $h2;
+        background: white;
+        border-radius: $h2/2;
+    }
+    // hover 白球往右
+    button:hover > span {
+        left: calc(100% - #{$h2} - 2px);
+    }
+</style>
