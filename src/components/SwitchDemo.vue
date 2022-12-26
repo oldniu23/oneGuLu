728392d2f7a19f1f38ec107b1b6a0755505f9e8a
oldniu <1241677287@qq.com>
2022-12-26 17:35:56 +0800

    switch组件 按钮状态切换

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <button><span></span></button>
+        <Switch />
     </div>
 </template>
 <script>
@@ -11,27 +11,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-    $h: 22px;
-    $h2: $h - 4px;
-    button{
-        height: $h;
-        width: $h*2;
-        border: none;
-        background: blue;
-        border-radius: $h/2;
-        position: relative;
-    }
-    span{
-        position: absolute;
-        top: 2px;
-        left: 2px;
-        height: $h2;
-        width: $h2;
-        background: white;
-        border-radius: $h2/2;
-    }
-    // hover 白球往右
-    button:hover > span {
-        left: calc(100% - #{$h2} - 2px);
-    }
+
 </style>
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,5 +1,50 @@
 <template>
     <div>
-        Switch 组件
+       <button @click="toggle" :class="{checked}"><span></span></button>
     </div>
 </template>
+
+<script lang="ts">
+import { ref } from 'vue'
+
+export default {
+    setup(){
+        // 常量x 用于控制class(checked) toggle用于切换x的value
+        const checked = ref(false)
+        const toggle = ()=>{
+            checked.value = !checked.value
+        }
+        return {checked,toggle}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    $h: 22px;
+    $h2: $h - 4px;
+    button{
+        height: $h;
+        width: $h*2;
+        border: none;
+        background: grey;
+        border-radius: $h/2;
+        position: relative;
+    }
+    span{
+        position: absolute;
+        top: 2px;
+        left: 2px;
+        height: $h2;
+        width: $h2;
+        background: white;
+        border-radius: $h2/2;
+    }
+    // checked 白球往右 背景变蓝
+    button.checked {
+        background: blue;
+    }
+    button.checked > span {
+        left: calc(100% - #{$h2} - 2px);
+    }
+    
+</style>
