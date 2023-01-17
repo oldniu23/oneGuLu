b2d1ef06b0ad1c9bba39472703090b3fd8e23fcd
oldniu <1241677287@qq.com>
2023-01-17 22:38:15 +0800

    button支持disabled禁用

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,6 +1,6 @@
 <template>
         <button class="gulu-button"
-            :class="classes">
+            :class="classes" :disabled="disabled">
             <slot />
         </button>      
 </template>
@@ -8,18 +8,24 @@
 import {computed} from 'vue'
 export default {
     props:{
-        theme:{
+        theme: {
             type: String,
             default: "button"
         },
-        size:{
+        size: {
             type: String,
             default: "normal"
         },
-        level:{
+        level: {
             type: String,
             default: "normal"
+        },
+        disabled: {
+            type: Boolean,
+            default: false
         }
+
+
     },
     setup(props){
         const { theme, size, level } = props;
@@ -42,6 +48,7 @@ $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
 $red: red;
+$grey: grey;
 
 .gulu-button {
     box-sizing: border-box;
@@ -138,6 +145,23 @@ $red: red;
             &:focus {
                 color: darken($red, 10%);
             }
+        }
+    }
+
+    &.gulu-theme-button {
+        &[disabled] {
+            // 禁用
+            cursor: not-allowed;
+            color: $grey;
+            &:hover {
+                border-color: $grey;
+            }
+        }
+    }
+    &.gulu-theme-link, &.gulu-theme-text {
+        &[disabled] {
+            cursor: not-allowed;
+            color: $grey;
         }
     }
 }
