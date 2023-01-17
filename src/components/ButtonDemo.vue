42c7e754b37d325807d2ed31f9d00ad82526735a
oldniu <1241677287@qq.com>
2023-01-17 22:24:33 +0800

    button支持level

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -23,6 +23,22 @@
         <Button theme="link">普普普</Button>
         <Button size="big" theme="link">大大大</Button>
     </div>
+    <h1>示例3</h1>
+    <div>
+        <Button level="main">主要按钮</Button>
+        <Button>普通按钮</Button>
+        <Button level="danger">危险按钮</Button>
+    </div>
+    <div>
+        <Button theme="text" level="main">主要文本按钮</Button>
+        <Button theme="text">普通文本按钮</Button>
+        <Button theme="text" level="danger">危险文本按钮</Button>
+    </div>
+    <div>
+        <Button theme="link" level="main">主要链接按钮</Button>
+        <Button theme="link" >普通链接按钮</Button>
+        <Button theme="link" level="danger">危险链接按钮</Button>
+    </div>
 </template>
 <script lang="ts">
 import Button from '../lib/Button.vue'
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -15,14 +15,19 @@ export default {
         size:{
             type: String,
             default: "normal"
+        },
+        level:{
+            type: String,
+            default: "normal"
         }
     },
     setup(props){
-        const { theme, size } = props;
+        const { theme, size, level } = props;
         const classes = computed(()=>{
             return {
                 [`gulu-theme-${theme}`]: theme,
-                [`gulu-size-${size}`]: size
+                [`gulu-size-${size}`]: size,
+                [`gulu-level-${level}`]: level,
             }
         })
         return {classes}
@@ -36,6 +41,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 
 .gulu-button {
     box-sizing: border-box;
@@ -86,6 +92,54 @@ $radius: 4px;
         height: 20px;
         padding: 0 4px;
     }
+
+    &.gulu-theme-button {
+        &.gulu-level-main {
+            background: $blue;
+            color: white;
+            border-color: $blue;
+            &:hover,
+            &:focus {
+                background: darken($blue, 10%);
+                border-color: darken($blue, 10%);
+            }
+        }
+        &.gulu-level-danger {
+            background: $red;
+            border-color: $red;
+            color: white;
+            &:hover,
+            &:focus {
+                background: darken($red, 10%);
+                border-color: darken($red, 10%);
+            }
+        }
     }
+    &.gulu-theme-link {
+        &.gulu-level-danger {
+            color: $red;
+            &:hover,
+            &:focus {
+                color: darken($red, 10%);
+            }
+        }
+    }
+    &.gulu-theme-text {
+        &.gulu-level-main {
+            color: $blue;
+            &:hover,
+            &:focus {
+                color: darken($blue, 10%);
+            }
+        }
+        &.gulu-level-danger {
+            color: $red;
+            &:hover,
+            &:focus {
+                color: darken($red, 10%);
+            }
+        }
+    }
+}
 
 </style>
