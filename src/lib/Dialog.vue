6b96ae33e41b139da97bf0dd34e56c599720cdc3
oldniu <1241677287@qq.com>
2023-01-18 11:09:35 +0800

    Dialog 创建dialog组件

---
+++ src/lib/Dialog.vue
@@ -0,0 +1,23 @@
+<template>
+    <!-- 遮罩层 -->
+    <div class="dialog-overlay"></div>
+    <!-- 内容区域 -->
+    <div class="dialog-wrapper">
+        <header>标题</header>
+        <main>
+            <p>第一行</p>
+            <p>第二行</p>
+        </main>
+        <footer>
+            <Button>关闭</Button>
+            <Button>确定</Button>
+        </footer>
+    </div>
+</template>
+
+<script lang="ts">
+import Button from "./Button.vue"
+export default {
+    components: { Button }
+}
+</script>
