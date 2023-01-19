0f075d4a17e440cf889a75fb16a59ad5a403ae36
oldniu <1241677287@qq.com>
2023-01-19 22:38:14 +0800

    Tabs-判断子标签类型/拿到子标签内容和title

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,5 +1,30 @@
 <template>
     <div>
-        Tabs 组件
+        <div v-for="(title, index) in titles" :key="index">{{title}}</div>
+        <!-- component标签 在此的作用类似插槽 -->
+        <component v-for="(c, index) in defaults" :is="c" :key="index"></component>
     </div>
 </template>
+
+<script>
+import Tab from "../lib/Tab.vue"
+export default {
+    components: { Tab },
+    setup(props, context) {
+        // console.log(context.slots.default(),"11");
+        const defaults = context.slots.default();
+        console.log(defaults,">>>>");
+        defaults.forEach((tag => {
+            // 防御性编程 防止tabs内出现tab以外的标签
+            if(tag.type !== Tab) {
+                throw new Error('Tabs的子标签必须是Tab')
+            }
+        }))
+        // 拿到tab标签的title
+        const titles = defaults.map((tag) => {
+            return tag.props.title
+        })
+        return { defaults, titles }
+    }
+}
+</script>
