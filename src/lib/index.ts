2f29488ff7e9fbc7b8cdf40d3e65d70c43fb5f53
oldniu <1241677287@qq.com>
2023-01-31 22:15:21 +0800

    创建lib/index.ts

---
+++ src/lib/index.ts
@@ -0,0 +1,7 @@
+// import Switch from './Switch.vue'   export {Switch}  合二为一就是下面
+export {default as Switch} from './Switch.vue'
+export {default as Button} from './Button.vue'
+export {default as Tabs} from './Tabs.vue'
+export {default as Tab} from './Tab.vue'
+export {default as Dialog} from './Dialog.vue'
+export {openDialog as openDialog} from './openDialog'
